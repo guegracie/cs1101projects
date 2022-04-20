18638242bfa2b1ae336745b75e87fa459aeca409
guegracie <95649024+guegracie@users.noreply.github.com>
2022-04-20 14:43:03 -0500

    Add database project
    This adds SQL queries and results for the project.

---
+++ games-database/games-released-birthyear-query.sql
@@ -0,0 +1,6 @@
+SELECT 
+        *
+FROM 
+        games
+WHERE 
+        first_release_date BETWEEN '2003-04-14 00:00:00' AND '2003-12-31 23:59:59';
---
+++ games-database/sandbox-create-update-delete-queries.sql
@@ -0,0 +1,15 @@
+INSERT INTO 
+   sandbox(int32_value, bool_value, string_value) 
+VALUES 
+   (148555, 0, 'hi')
+
+
+UPDATE 
+    sandbox 
+SET
+    int16_value = 20 
+    datetime_value = '2100-12-22 00:00:00'
+WHERE 
+ id = 5
+
+DELETE FROM sandbox WHERE id = 9 
